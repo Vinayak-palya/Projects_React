213eaf75dec49dcafc585778731c242f088f82bb
Vinayak-Palya <vinayakpalya2704@gmail.com>
2024-10-14 14:37:46 +0530

    change

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import UserContextProvider from './context/userContextProvider'
 
 function App() {
  
   return (
-    <>
-      react with chai
-    </>
+    <UserContextProvider>
+      react 
+    </UserContextProvider>
   )
 }
 
